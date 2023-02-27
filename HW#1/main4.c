2cf28c34f4751e173db6ce87729e06a17e93ca5c
B1129005 <113523539+zchsu@users.noreply.github.com>
2023-02-27 21:50:46 +0800

    Update main4.c

--- HW#1/main4.c
+++ HW#1/main4.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    char str[] = "ac-99bc";
+    char a[] = "ac-99bc";
     int i = 0, sign = 1, num = 0;
     
-    while (str[i] != '\0') {
-        if (str[i] == '-') {  // 檢查負號
+    while (a[i] != '\0') {
+        if (a[i] == '-') {  // 檢查負號
             sign = -1;
         }
         i++;
     }    
     i=0;
     
-    while (str[i] != '\0') {  
-        if (str[i] >= '0' && str[i] <= '9') {  
-            num = num * 10 + (str[i] - '0');  // 計算整數
+    while (a[i] != '\0') {  
+        if (a[i] >= '0' && a[i] <= '9') {  
+            num = num * 10 + (a[i] - '0');  // 計算整數
         } 
         i++;
     }
