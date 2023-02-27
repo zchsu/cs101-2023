0f4873df2cca81eca4ec58567b7a38e819d9b724
B1129005 <113523539+zchsu@users.noreply.github.com>
2023-02-27 19:55:21 +0800

    Update main2.c

--- HW#1/main2.c
+++ HW#1/main2.c
@@ -10,11 +10,11 @@ int main() {
         int count = 0;
         while (a[i] >= '0' && a[i] <= '9') {
             count = a[i] - '0';
-            i++;
             for (int k = 0; k < count; k++) {
-                output[j] = a[i-2];
+                output[j] = a[i-1];
                 j++;
             }
+            i++;
         }
     }
     output[j] = '\0'; 
