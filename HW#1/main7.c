b4ebfc04ce0b441d78fcd3d7823bc3f61145138c
B1129005 <113523539+zchsu@users.noreply.github.com>
2023-02-28 23:38:52 +0800

    Update main7.c

--- HW#1/main7.c
+++ HW#1/main7.c
@@ -4,29 +4,22 @@ int main() {
     char a[] = "10001111";
     char str[100];
     int decimal = 0;
+    int len = (sizeof(a) / sizeof(char) - 1) / 4;
     
     int i, j;
-    for(i = 0; a[i] != '\0'; i++) {     //轉十進位
+    for (i = 0; a[i] != '\0'; i++) {     //轉十進位
         decimal = decimal * 2 + (a[i] - '0');
     }
-    i = 0;
-    
+    if (decimal == 0) {   //如果輸入為00000000
+        printf("%d", decimal);
+    }
+
     while (decimal != 0) {    //計算十六進位
-        int r = decimal % 16;
-        str[i++] = (r < 10)?r + '0':r - 10 + 'A';
+        int r = decimal % 16;   
+        str[len - 1] = (r < 10)?r + '0':r - 10 + 'A';
         decimal /= 16;
-    }
-    if (i == 0) {
-        str[i++] = '0';
-    }
-    str[i] = '\0';
-    
-    int len = i;
-    for (i = 0, j = len - 1; i < j; i++, j--) {
-        char temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
-    }
+        len--;
+    }  
     printf("%s\n", str);
     return 0;
 }
