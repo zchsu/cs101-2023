2ccece25fd3a4573bedcfb4bbac1cb078175bcfc
B1129005 <113523539+zchsu@users.noreply.github.com>
2023-02-28 23:11:34 +0800

    Update main8.c

--- HW#1/main8.c
+++ HW#1/main8.c
@@ -1,36 +1,32 @@
 #include <stdio.h>
 
-void swap(char *a, char *b) {
-    char temp = *a;
-    *a = *b;
-    *b = temp;
-}
-
-void reverse(char str[], int len) {
-    int i, j;
-    for (i = 0, j = len; i < j; i++, j--) {    // 交換字母
-        
-        while (!((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z')) && i < j) i++;
-        while (!((str[j] >= 'a' && str[j] <= 'z') || (str[j] >= 'A' && str[j] <= 'Z')) && i < j) j--;
-        swap(&str[i], &str[j]);
-    }
-}
-
-void change(char str[], int len) {
-    if (str[0] >= 'a' && str[0] <= 'z') {
-            str[0] = str[0] - 'a' + 'A';
-    }
-    if (str[len - 1] >= 'A' && str[len - 1] <= 'Z') {
-            str[len - 1] = str[len - 1] - 'A' + 'a';
-    }
-}
-
 int main() {
-    char a[] = "Hello";
-    int len = sizeof(a) / sizeof(a[0]) - 1;
-    printf("a: %s\n", a);
-    reverse(a, len);
-    change(a, len);
-    printf("a: %s\n", a);
+    char a[] = "10001111";
+    char str[100];
+    int decimal = 0;
+    
+    int i, j;
+    for(i = 0; a[i] != '\0'; i++) {     //轉十進位
+        decimal = decimal * 2 + (a[i] - '0');
+    }
+    i = 0;
+    
+    while (decimal != 0) {    //計算十六進位
+        int r = decimal % 16;
+        str[i++] = (r < 10)?r + '0':r - 10 + 'A';
+        decimal /= 16;
+    }
+    if (i == 0) {
+        str[i++] = '0';
+    }
+    str[i] = '\0';
+    
+    int len = i;
+    for (i = 0, j = len - 1; i < j; i++, j--) {
+        char temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+    printf("%s\n", str);
     return 0;
 }
