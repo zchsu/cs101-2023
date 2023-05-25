7c7864f5fa3fe7092ab926c88f41264359144d30
B1129005 <113523539+zchsu@users.noreply.github.com>
2023-05-25 14:51:05 +0800

    Update main.cpp

--- Lab#9/main.cpp
+++ Lab#9/main.cpp
@@ -1,32 +1,46 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
-using namespace std;
-
-class ReplaceMystring{
-    public:
-    void replaceString(string& str){
-        ifstream in;
-    ofstream out;
-    char str;
-    in.open("main.cpp");
-    out.open("rmain.cpp");
-    while(!in.eof()){
-        in>>str;
-        if(in.fail()){
-            
-        }
-        out << str << endl;
-            if(str=='\n'){
-                out << endl;
-            }
-    }
-    }
-};
-int main()
-{
-    ReplaceMystring my;
-    my.replaceString("IU","IU is best");
-    return 0;
-}
+#include <iostream> 
+ #include <fstream> 
+ #include <string> 
+  
+ using namespace std; 
+  
+ class ReplaceMyString{ 
+ public: 
+         void replaceString(string s1, string s2){ 
+                 string str; 
+                 ifstream in("main.cpp"); 
+                 ofstream out("rmain.cpp"); 
+                 while(!in.eof()){ 
+                         getline(in, str); 
+                         for(int i=0 ; i < str.size() ; i++){ 
+                                 string sub = str.substr(i,s1.size()); 
+                                 if(sub == s1) 
+                                         str.replace(str.begin()+i, str.begin()+i+2, s2); 
+                         } 
+                         out << str << endl; 
+                 } 
+         } 
+ }; 
+  
+ int main(){ 
+         ReplaceMyString my; 
+         my.replaceString("IU", "IU is best"); 
+         return 0; 
+ }
+   
+    
+   
+        
+        
+     
+   
+   
+    
+   
+       
+       
+              
+          
+                
+         
+ 
